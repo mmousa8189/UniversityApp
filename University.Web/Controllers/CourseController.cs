dd4ff5b03cbffe21503de58d2a248699f59d3440
Mohamed Mousa <mohamedamousa00@gmail.com>
2021-08-25 15:11:48 +0200

    add API project

---
+++ University.Web/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using University.DataAccess.Models;
+using University.DataAccess.Repository;
+
+namespace University.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly IRepository _repository;
+
+        public CourseController(IRepository repository)
+        {
+            _repository = repository;
+        }
+        public IActionResult Index()
+        {
+           var courses = _repository.GetAll<Course>().ToList();
+            return View();
+        }
+    }
+}
